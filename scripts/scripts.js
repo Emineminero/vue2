804f0c7f0034b470855dfa1bc6504f11c314dedf
Javier Laurenzano <jlaurenzano@altimetrik.com>
2017-02-21 09:49:59 -0300

    aa

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,4 +3,15 @@ var app = new Vue({
   data: {
     message: 'Hello Vue!'
   }
+});
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
 })
